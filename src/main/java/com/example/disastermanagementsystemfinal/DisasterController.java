7fba4098fe1e816fe44ccccf720fe9a7a7f39f97
Inori166 <fibnshahid@gmail.com>
2023-04-28 22:42:27 +0600

    Doing

--- src/main/java/com/example/disastermanagementsystemfinal/DisasterController.java
+++ src/main/java/com/example/disastermanagementsystemfinal/DisasterController.java
@@ -30,4 +30,13 @@ public class DisasterController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchtoPrevDisaster(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("PrevDisaster.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/disastermanagementsystemfinal/Main.java
+++ src/main/java/com/example/disastermanagementsystemfinal/Main.java
@@ -11,16 +11,12 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException{
-        try{
+
             Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
             Scene scene = new Scene((root));
             stage.setScene(scene);
             stage.show();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+
     }
 
 
